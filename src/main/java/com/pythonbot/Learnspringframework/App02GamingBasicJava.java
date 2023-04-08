0f7fa5d61a76e9dddbc4e7a81c26cf4bfe5ac6da
Ashish <anshm9448@gmail.com>
2023-04-08 19:12:10 +0530

    Combined Gaming basic and configuaration

--- src/main/java/com/pythonbot/Learnspringframework/App02GamingBasicJava.java
+++ src/main/java/com/pythonbot/Learnspringframework/App02GamingBasicJava.java
@@ -1,7 +1,27 @@
 package com.pythonbot.Learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+class GamingConfiguaration {
+    @Bean
+    public GamingConsole game(){
+        return new PacmanGame();
+    }
+    @Bean
+    public GameRunner gameRunner(GamingConsole game){
+        //var game= new PacmanGame();
+        return new GameRunner(game);
+    }
+    //var game= new SuperContraGame();
+    //var game= new MarioGame();
+    //  var game= new PacmanGame();//1. Object Creation
+    // var gameRunner= new GameRunner(game);//2. object creation + wiring of Dependencies
+    //Game is a Dependency
+    //     gameRunner.run();
+}
 public class App02GamingBasicJava {
     public static void main(String[] args) {
         try(var context=
